91b6866d093ba9f1f4acc57311ce7ea12a0be483
hallel2118 <hallel2118@outlook.jp>
2020-10-12 14:34:27 +0900

    learn jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-    return (<div><h1>Hello,world!</h1></div>);
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <label htmlFor="bar">bar</label>
+        <input type="text" onChange={() => {console.log("I am clicked")}} />
+      </React.Fragment>
+//    const greeting = "Hi, Tom!";
+//    const dom = <h1 className="foo">{greeting}</h1>;
+//    return <h1>Hello,world!</h1>;
+//    return dom;
+    )
+  }
 }
+
 export default App;
